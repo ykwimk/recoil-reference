57cbb2a754b91490752f6c49823d9f36b2349e2d
ykwimk <ykwimk@gmail.com>
2022-02-21 20:10:51 +0900

    Fix: Suspense -> Loadable 로직 수정

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValueLoadable } from 'recoil';
 import { getListSelector } from '../../recoil/list';
 import ListItem from '../ListItem';
 import { ListWrapper } from './List.style';
 
 const List = () => {
-  const list = useRecoilValue(getListSelector);
+  const listLoadable = useRecoilValueLoadable(getListSelector);
 
-  return (
-    <ListWrapper>
-      {list.map((item: any) => {
-        const { id, name, image_url } = item;
-        return <ListItem key={id} name={name} imageUrl={image_url} />;
-      })}
-    </ListWrapper>
-  );
+  switch (listLoadable.state) {
+    case 'hasValue': {
+      return (
+        <ListWrapper>
+          {listLoadable.contents.map((item: any) => {
+            const { id, name, image_url } = item;
+            return <ListItem key={id} name={name} imageUrl={image_url} />;
+          })}
+        </ListWrapper>
+      );
+    }
+    case 'loading': {
+      return <div>loading...</div>;
+    }
+    case 'hasError': {
+      throw listLoadable.contents;
+    }
+  }
 };
 
 export default List;
